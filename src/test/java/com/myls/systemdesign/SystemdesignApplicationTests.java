20ad5e57ae53057f32bd1c0cb949a5debc634849
boyalapalliRambabu <boyalapallirambabu4@gmail.com>
2026-08-11 10:09:11 +0530

    First commit

---
+++ src/test/java/com/myls/systemdesign/SystemdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myls.systemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
